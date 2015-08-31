b2c678e1914dc52b2c920da173054c731ba8b11c
Jacek Marchwicki <jacek.marchwicki@gmail.com>
2015-08-31 17:03:30 +0200

    Added javadoc to new classes
    Change-Id: I1657d9f5571f18bf515858f017a5c28fbe257f67

--- websockets-rxjava/src/main/java/com/appunite/websocket/rx/object/ObjectParseException.java
+++ websockets-rxjava/src/main/java/com/appunite/websocket/rx/object/ObjectParseException.java
@@ -1,14 +1,42 @@
+/*
+ * Copyright (C) 2015 Jacek Marchwicki <jacek.marchwicki@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
 package com.appunite.websocket.rx.object;
 
+/**
+ * Exception throw by {@link ObjectSerializer} if serialization or deserialization fail
+ */
 public class ObjectParseException extends Exception {
 
+    /**
+     * {@inheritDoc}
+     */
     public ObjectParseException() {
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public ObjectParseException(String message) {
         super(message);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public ObjectParseException(String message, Throwable cause) {
         super(message, cause);
     }
@@ -17,6 +45,9 @@ public class ObjectParseException extends Exception {
         super(cause);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public ObjectParseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
